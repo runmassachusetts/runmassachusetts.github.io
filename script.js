1752accca21e77246713e83e76d2c06737343925
runmassachusetts <max@runmassachusetts.com>
2026-06-23 12:12:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-// Create map centered on a location
-const map = L.map('map').setView([42.65, -71.13], 10); // Lawrence, MA area
+const map = L.map('map').setView([42.3, -71.8], 8);
 
-// Add OpenStreetMap tiles
+// Base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker
-L.marker([42.65, -71.13]).addTo(map)
-  .bindPopup('Hello from Lawrence!')
-  .openPopup();
+// Example: towns you've run in
+const runTowns = {
+  "Boston": true,
+  "Lowell": true,
+  "Cambridge": true,
+  "Lawrence": true
+};
 
-// Add another clickable marker
-L.marker([42.7, -71.15]).addTo(map)
-  .bindPopup('Another location');
+// GoDaddy base URL (CHANGE THIS)
+const baseUrl = "https://runmassachusetts/";
+
+// Load MA towns
+fetch("massachusetts.geojson")
+  .then(res => res.json())
+  .then(data => {
+
+    L.geoJSON(data, {
+      style: function (feature) {
+        const name = feature.properties.name;
+
+        const hasRun = runTowns[name];
+
+        return {
+          color: "#333",
+          weight: 1,
+          fillColor: hasRun ? "green" : "#cccccc",
+          fillOpacity: hasRun ? 0.6 : 0.2
+        };
+      },
+
+      onEachFeature: function (feature, layer) {
+        const town = feature.properties.name;
+
+        layer.on("click", function () {
+          const url = `${baseUrl}/${town.toLowerCase()}`;
+          window.location.href = url;
+        });
+
+        layer.bindPopup(town);
+      }
+    }).addTo(map);
+  });
